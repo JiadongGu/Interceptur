7a1720477f83557d9ce31a906b509eabf2755301
JiadongGu <108995699+JiadongGu@users.noreply.github.com>
2022-09-21 18:10:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/")
+@app.route("/index")
 def index():
     if request.method == "POST":
         rqs = request.form.get("id")
